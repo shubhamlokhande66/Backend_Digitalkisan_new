c54031d75d0d0064cd068a0e737951ef22b4cb5e
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2023-03-15 10:24:19 +0530

    adding header

--- models/user.controller.js
+++ models/user.controller.js
@@ -11,6 +11,16 @@ const { Otp } = require('../models/opt.model');
 
     module.exports.signUp = async (req, res) => {
 
+        res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+
         const user = await User.findOne({
             number: req.body.number
         });
